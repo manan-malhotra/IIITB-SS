c90db1b24244455f58773c7fc8ca3516734e724b
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-08-29 23:15:43 +0530

    Updated Qs 18

--- hands_on_list1/18/threeRecords.c
+++ hands_on_list1/18/threeRecords.c
@@ -1,52 +1,131 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
 #include<fcntl.h>
-#include<stdlib.h>
+#include<unistd.h>
 #include<string.h>
+#include<stdlib.h>
 
-struct resident{
-  int roll_no;
-  char addr[20];
-  int marks;
+void read_lock(int lock_r);
+void write_lock(int lock_r);
+
+struct student {
+    int roll_no;
+    int marks;
 };
 
-int main() {
-  struct flock lock;
-  int fd=open("rec.txt", O_CREAT | O_RDWR, 0744);
-  struct resident res1, res2, res3;
-  res1.roll_no=1;
-  res2.roll_no=2;
-  res3.roll_no=3;
-  strcpy(res1.addr, "ABC");
-  strcpy(res2.addr, "XYZ");
-  strcpy(res3.addr, "GHI");
-  res1.marks=18;
-  res2.marks=16;
-  res3.marks=9;
-  write(fd, &res1, sizeof(struct resident));
-  lock.l_type=F_WRLCK;
-  lock.l_whence = SEEK_SET;
-  lock.l_start = 8;
-  lock.l_len = 8;
-  lock.l_pid = getpid();
-  fcntl(fd,F_SETLKW , &lock);
-  printf("Entered critical section...\n");
-  printf("Press return to unlock...\n");
-  getchar();
-  printf("Write lock unset...\n");
-  lock.l_type=F_RDLCK;
-  lock.l_start=0;
-  lock.l_len=8;
-  fcntl(fd, F_SETLKW, &lock);
-  printf("Read lock set...\n");
-  lock.l_type=F_UNLCK;
-  lock.l_start=8;
-  lock.l_len=8;
-  fcntl(fd, F_SETLKW, &lock);
-  printf("Press return to exit...\n");
-  getchar();
-  printf("Read lock unset...\n");
-  close(fd);
-return 0;
+void main() {
+    int fd = open("file18.txt", O_RDWR | O_CREAT, 0666);
+    if (fd < 0) {
+		perror("file");
+		exit(1);
+	}
+    struct student inp1 = {1, 10};
+    struct student inp2 = {2, 20};
+    struct student inp3 = {3, 30};
+
+    write(fd, &inp1, sizeof(struct student));
+    write(fd, &inp2, sizeof(struct student));
+    write(fd, &inp3, sizeof(struct student));
+
+    close(fd);
+
+    fd=open("file18.txt", O_RDONLY);
+    if (fd < 0) {
+		perror("file");
+		exit(1);
+	}
+    struct student std;
+    read(fd, &std, sizeof(struct student));
+    printf("Roll No. : %d \n", std.roll_no);
+    printf("Marks : %d \n \n", std.marks);
+
+    read(fd, &std, sizeof(struct student));
+    printf("Roll No. : %d \n", std.roll_no);
+    printf("Marks : %d \n \n", std.marks);
+
+    read(fd, &std, sizeof(struct student));
+    printf("Roll No. : %d \n", std.roll_no);
+    printf("Marks : %d \n \n", std.marks);
+
+    int lock_r;
+    int choice;
+    lseek(fd, 0, SEEK_CUR);
+    printf("Select record you want to lock: ");
+    scanf("%d", &lock_r);
+    close(fd);
+    if(lock_r < 1 || lock_r > 3) exit(1);
+    printf("Select type of lock: \nRead (1)\nWrite (2) \nChoice: ");
+    scanf("%d", &choice);
+    if(choice == 1) {
+        read_lock(lock_r);
+        exit(1);
+    }
+    else if(choice == 2) {
+        write_lock(lock_r);
+        exit(1);
+    }
+    else {
+        printf("Wrong choice!");
+    }
+    close(fd);
+
+}
+
+void read_lock(int lock_r) {
+    int fd=open("file18.txt", O_RDONLY);
+    struct student std;
+    lseek(fd, (lock_r - 1) * sizeof(struct student), SEEK_CUR);
+    read(fd, &std, sizeof(struct student));
+    printf("Roll No: %d \nMarks : %d \n", std.roll_no, std.marks);
+    close(fd);
+    fd=open("file18.txt", O_RDWR);
+    if(fd < 0) perror("file");
+    struct flock lock;
+    lock.l_type=F_RDLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start =  ( lock_r - 1 ) * sizeof(struct student);
+    lock.l_len = sizeof(struct student);
+    printf("Waiting to acquire lock on record %d \n", std.roll_no);
+    fcntl(fd, F_SETLKW, &lock);
+    printf("Acquired lock on record %d \n", std.roll_no);
+    printf("Press return to exit...\n");
+    getchar();
+    getchar();
+    lock.l_type = F_UNLCK;
+    fcntl(fd, F_SETLKW, &lock);
+    printf("Exited critical section...\n");
+}
+
+void write_lock(int lock_r) {
+    int fd=open("file18.txt", O_RDONLY);
+    if(fd < 0) perror("file");
+    struct student std;
+    lseek(fd, ( lock_r  - 1) * sizeof(struct student), SEEK_CUR);
+    read(fd, &std, sizeof(struct student));
+    printf("Roll No: %d \nMarks : %d \n", std.roll_no, std.marks);
+    close(fd);
+    fd=open("file18.txt", O_RDWR);
+    if(fd < 0) perror("file");
+    struct flock lock;
+    lseek(fd, ( lock_r  - 1) * sizeof(struct student), SEEK_CUR);
+    lock.l_type=F_RDLCK;
+    lock.l_whence=SEEK_SET;
+    lock.l_start=(lock_r - 1) * sizeof(struct student);
+    lock.l_len = sizeof(struct student);
+    printf("Waiting to acquire lock on record %d \n", std.roll_no);
+    lock.l_type=F_WRLCK;
+    int r=fcntl(fd, F_SETLKW, &lock);
+    if(r < 0) perror("lock");
+    printf("Acquired lock on record %d \n", std.roll_no);
+    printf("You selected to write on this record. \nEnter new marks: ");
+    int marks;
+    scanf("%d", &marks);
+    std.marks=marks;
+    lseek(fd, -1 * sizeof(struct student), SEEK_CUR);
+    write(fd, &std, sizeof(struct student));
+    printf("Press return to exit...\n");
+    getchar();
+    getchar();
+    lock.l_type = F_UNLCK;
+    fcntl(fd, F_SETLKW, &lock);
+    printf("Exited critical section...\n");
 }
