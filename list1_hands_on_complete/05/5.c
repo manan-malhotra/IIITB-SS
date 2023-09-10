20fae31473a6bdb2188e0dda958bbb0bc6c72904
Manan Malhotra <60249740+manan-malhotra@users.noreply.github.com>
2023-09-10 15:50:17 +0530

    Changes for open

--- list1_hands_on_complete/05/5.c
+++ list1_hands_on_complete/05/5.c
@@ -9,6 +9,7 @@ Date: 23rd Aug, 2023.
 #include<stdio.h>
 #include<fcntl.h>
 #include<stdbool.h>
+#include<unistd.h>
 int main(){
 int k = 0;
 FILE* file;
@@ -17,8 +18,10 @@ char buffer[32];
   if(k<5) {
   k++;
   snprintf(buffer, sizeof(char) * 32, "file%i.txt", k);
-  file = fopen(buffer, "wb");
-  fclose(file);
+//  file = fopen(buffer, "wb");
+//  fclose(file)
+int fd = open(buffer,O_CREAT,0666);
+close(fd);
 }
 }
 return 0;
